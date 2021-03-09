642787da7858a8cbfe1fc4a1b310b01592be0985
kukunbaiquni <kukuh.baiquni@gmail.com>
2021-03-09 22:32:16 +0700

    scroll update

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,59 @@ import './App.css'
 
 function App() {
   return (
-    <div className='App'>
+    <div className='App' style={{ height: 1000 }}>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <div style={{ width: 100, overflow: 'auto' }}>
+          RDD            RDD
+          RDD
+          RDD
+          RDD
+          RDD
+          RDD
+          RDD
+          RDD
+          RDD
+          RDD
+          RDD
+          RDD
+          RDD
+          RDD
+          RDD
+          RDD
+          RDD
+          RDD
+          RDD
+          RDD
+          RDD
+          RDD
+          RDD
+          RDD
+          RDD
+          RDD
+          RDD
+          RDD
+          RDD
+          ARC
+          ARC
+          ARC
+          ARC
+          ARC
+          ARC
+          ARC
+          ARC
+          ARC
+          ARC
+          ARC
+          ARC
+          ARC
+          ARC
+
+        </div>
         <a
           className='App-link'
           href='https://reactjs.org'
